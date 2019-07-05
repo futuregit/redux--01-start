90846fe5f93057fd927c084a4ebdf29b8aabc19a
futuregit <gmail.com>
2019-07-04 22:06:39 -0400

    Added type and action

--- redux-basics.js
+++ redux-basics.js
@@ -2,19 +2,39 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const initialState = {
-  counter : "WOW, this redux works!"
-}
+  counter : 0
+};
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+  if(action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  };
+    if(action.type === "ADD_COUNTER") {
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
+  };
   return state;
 };
 
 // Store
 const store = createStore(rootReducer);
-console.log("store.getState", store.getState());
-
+console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+console.log(store.getState());
+
+store.dispatch({
+  type: 'ADD_COUNTER',
+  value: 10
+});
+console.log(store.getState());
+
 
 // Subscription
