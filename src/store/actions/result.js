b389a1efba991075d269eb1d408373984b706ce2
futuregit <gmail.com>
2019-07-13 23:22:36 -0400

    Outsourced parts of reducer logic

---
+++ src/store/actions/result.js
@@ -0,0 +1,24 @@
+import * as actionTypes from './actionTypes';
+
+export const saveResult = (result) => {
+  // const updatedResult = result * 2;
+  return {
+    type: actionTypes.STORE_RESULT,
+    result: result
+  };
+}
+export const store_result = (result) => {
+  return  (dispatch, getState) => {
+    setTimeout(() => {
+      // const oldCounter = getState().ctr.counter;
+      // console.log("oldCounter", oldCounter)
+      dispatch(saveResult(result));
+    }, 2000)
+  }
+};
+export const delete_result = (id) => {
+  return {
+    type: actionTypes.DELETE_RESULT,
+    id: id
+  }
+};
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updatedObject } from '../utility';
 
 const initialState = {
   results: []
@@ -8,19 +9,13 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
       // Change data
-      return {
-        ...state,
-        results: state.results.concat({id: new Date(), value: action.result})
-        }
+      return updatedObject(state, { results: state.results.concat({id: new Date(), value: action.result})});
       case actionTypes.DELETE_RESULT:
         // const id = 2;
         // const newArray = [...state.results]
         // newArray.splice(id, 1);
-        const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-        return {
-          ...state,
-          results: updatedArray
-          }
+        const updatedArray = state.results.filter(result => result.id !== action.id);
+        return updatedObject(state, {  results: updatedArray });
       default:
         return state;
     };
