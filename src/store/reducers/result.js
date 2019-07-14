5defa2d36ad574474bc0bb1c5281b688f77fa29d
futuregit <gmail.com>
2019-07-14 15:22:50 -0400

    Made leaner switch statement in reducer

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -3,22 +3,19 @@ import { updatedObject } from '../utility';
 
 const initialState = {
   results: []
-}
+};
+
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(result => result.id !== action.id);
+  return updatedObject(state, {  results: updatedArray });
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.STORE_RESULT:
-      // Change data
-      return updatedObject(state, { results: state.results.concat({id: new Date(), value: action.result})});
-      case actionTypes.DELETE_RESULT:
-        // const id = 2;
-        // const newArray = [...state.results]
-        // newArray.splice(id, 1);
-        const updatedArray = state.results.filter(result => result.id !== action.id);
-        return updatedObject(state, {  results: updatedArray });
-      default:
-        return state;
+      case actionTypes.STORE_RESULT: return updatedObject(state, { results: state.results.concat({id: new Date(), value: action.result})});
+      case actionTypes.DELETE_RESULT: return deleteResult(state, action);
     };
+      return state;
   };
 
 export default reducer;
